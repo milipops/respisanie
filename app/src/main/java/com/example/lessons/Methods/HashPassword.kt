0360eb1ef8fa0c449789b577a8ae4c830f094e02
PigeonKur <ggnn3974@gmail.com>
2025-05-21 17:55:46 +0300

    add func hashPass

---
+++ app/src/main/java/com/example/lessons/Methods/HashPassword.kt
@@ -0,0 +1,10 @@
+package com.example.lessons.Methods
+
+import java.security.MessageDigest
+
+fun hashPassword(password: String): String {
+    val bytes = password.toByteArray()
+    val md = MessageDigest.getInstance("SHA-256")
+    val digest = md.digest(bytes)
+    return digest.joinToString("") { "%02x".format(it) }
+}
